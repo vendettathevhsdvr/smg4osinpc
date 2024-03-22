8756c910a50dcfb9d364dd50e6d262c69696646d
vendettathevhsdvr <98217346+vendettathevhsdvr@users.noreply.github.com>
2024-03-22 20:41:15 +0500

    Add files via upload

---
+++ SMG4 OS IN PC/Form7.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SMG4_OS_IN_PC
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form7));
+            label1 = new System.Windows.Forms.Label();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(43, 41);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(56, 15);
+            label1.TabIndex = 0;
+            label1.Text = "SMG4 OS";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.SMG4_Os_My_Menu;
+            pictureBox1.Location = new System.Drawing.Point(171, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(100, 96);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(28, 137);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(142, 15);
+            label2.TabIndex = 2;
+            label2.Text = "CPU:SMG4 Core I5 1488-4";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(28, 166);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(71, 15);
+            label3.TabIndex = 3;
+            label3.Text = "RAMs:10 GB";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(28, 189);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(170, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Video Card:SMG4 Geforce 2024";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(28, 213);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(184, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Sound Card:SMG4 Sound Card 4K";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(28, 239);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(155, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Modem:SMG4 Modem 1488";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(28, 268);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(130, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Monitor:SMG4 Monitor";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new System.Drawing.Point(30, 302);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(185, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Hard Disk:SMG4 Hard Disk 959 GB";
+            // 
+            // Form7
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(283, 361);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "Form7";
+            Text = "My Menu";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
